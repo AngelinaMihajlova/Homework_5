3dff4987b21d4f605e89fe618857f720ff22e208
AngelinaMihajlova <mihajlovaangelina@yandex.ru>
2022-07-15 20:11:56 +0300

    Внесены изменения

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -1,21 +1,17 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива
 
-    double[] Array = new double[5] {2, 35, 150, 96,4};
+double[] array = new double[5] {3.57, 35.5, 150, 96.2, 4};
 
-    double max = Array[0];
-    double min = Array[0];
-    double result = 0;
+double max = array[0];
+double min = array[0];
+double result;
    
-    for (int i = 0; i < Array.Length; i++)
-    {
-        if (Array[i] > max) 
-        {   max = Array[i];
-        }
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)  max = array[i];
+    if (array[i] < min)  min = array[i];
+} 
 
-        if (Array[i] < min)
-        {   min = Array[i];
-        } 
-    }
 result = max - min;
 
 Console.WriteLine (result);
